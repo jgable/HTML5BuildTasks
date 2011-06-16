d0487e4162f2c2bc95ddf2cef5aed46d1d37ef9a
jacob4u2 <jacob.gable@gmail.com>
2011-06-16 13:17:20 -0500

    Adding JSMin and CSSMin implementations

---
+++ HTML5BuildTasks/CSSMin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HTML5BuildTasks
+{
+    /// <summary>
+    /// A really, really basic regex based CSS Minifier.
+    /// </summary>
+    public class CSSMin
+    {
+        public string MinifyCSS(string css)
+        {
+            Regex.Replace(css, "/\\*.+?\\*/", "", RegexOptions.Singleline);
+            css = css.Replace("  ", string.Empty);
+            css = css.Replace(Environment.NewLine + Environment.NewLine + Environment.NewLine, string.Empty);
+            css = css.Replace(Environment.NewLine + Environment.NewLine, Environment.NewLine);
+            css = css.Replace(Environment.NewLine, string.Empty);
+            css = css.Replace("\\t", string.Empty);
+            css = css.Replace(" {", "{");
+            css = css.Replace(" :", ":");
+            css = css.Replace(": ", ":");
+            css = css.Replace(", ", ",");
+            css = css.Replace("; ", ";");
+            css = css.Replace(";}", "}");
+            css = Regex.Replace(css, "/\\*[^\\*]*\\*+([^/\\*]*\\*+)*/", "$1");
+            css = Regex.Replace(css, "(?<=[>])\\s{2,}(?=[<])|(?<=[>])\\s{2,}(?=&nbsp;)|(?<=&ndsp;)\\s{2,}(?=[<])", string.Empty);
+    
+            return css;
+        }
+    }
+}
